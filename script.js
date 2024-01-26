0565d1c42d084cb311e7e743f5edbfd655e7ee32
Akshay Taware <97909906+akshaytaware13@users.noreply.github.com>
2024-01-26 16:20:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+const buttons = document.querySelectorAll(".button");
+
+const body = document.querySelector("body");
+
+buttons.forEach((button) => {
+  console.log(button);
+  button.addEventListener("click", function (e) {
+    console.log(e);
+    console.log(e.target);
+
+    if (e.target.id === "red") {
+      body.style.backgroundColor = e.target.id;
+      body.style.color = "white";
+    } else if (e.target.id === "green") {
+      body.style.backgroundColor = e.target.id;
+      body.style.color = "white";
+    } else if (e.target.id === "blue") {
+      body.style.backgroundColor = e.target.id;
+      body.style.color = "white";
+    } else {
+      e.target.id === "yellow";
+      body.style.backgroundColor = e.target.id;
+      body.style.color = "grey";
+    }
+  });
+});
+ 
